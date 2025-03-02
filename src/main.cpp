5f3a2118350a8cc7dd34b3b8b5048fc9b3d867c5
HouJianyuH <109130120+HouJianyuH@users.noreply.github.com>
2025-03-02 21:35:34 +0800

    Update main.cpp
    2025,3,2

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,17 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3_image/SDL_image.h>
-#include <box2d/b2_body.h>
-#include <box2d/b2_fixture.h>
+#include <algorithm>
+#include <box2d/b2_common.h>
 #include <box2d/b2_math.h>
-#include <box2d/b2_world.h>
 #include <box2d/box2d.h>
-#include <cstddef>
 #include <cstdio>
-
+#include <vector>
+#include "LTE.h"
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
 
-struct Camera{
-	Camera(int _x, int _y, int _w, int _h, int _zoom) { 
-        x = _x; y = _y; w = _w; h = _h; zoom = _zoom;
-    }
-	int x, y, w, h, zoom;
-}camera(0,0, WINDOW_WIDTH, WINDOW_HEIGHT,30);
+Camera camera(0,0, WINDOW_WIDTH, WINDOW_HEIGHT,64);
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
@@ -30,41 +24,70 @@ bool keep = true;
 b2Vec2 gravity(0.0f, -10.0f);
 b2World world(gravity);
 float timeStep = 1.0f / 60.0f;
-int32 velocityIterations = 6;
-int32 positionIterations = 2;
+int32 velocityIterations = 8;
+int32 positionIterations = 3;
 struct Object{
     //构造函数
     Object( b2World* world,
-            float _x = 0.0f, float _y = 0.0f, 
-            float _w = 1.0f, float _h = 1.0f,
-            float _density = 1.0f ,float _friction = 0.3f){
-
+            b2Vec2 position,
+            int32 _count,
+            b2Vec2 _vertices[],
+            float _density,float _friction
+            ){
+        
         bodyDef.type = b2_dynamicBody;//动态身体
-        bodyDef.position.Set(_x, _y);
+        bodyDef.position.Set(position.x, position.y);
         body = world->CreateBody(&bodyDef);
-        Shape.SetAsBox(_w,_h);
+        
+        for (int i=0; i < _count; i++)
+            vertices.push_back(_vertices[i]);
+        count = _count;
+        Shape.Set(_vertices,_count);
 
         fixtureDef.shape = &Shape;
         fixtureDef.density = _density;   //密度
         fixtureDef.friction = _friction; //摩擦
         body->CreateFixture(&fixtureDef);
     }
+
     b2BodyDef bodyDef;
     b2Body* body;
-
-    b2PolygonShape Shape;
     
+    std::vector<b2Vec2> vertices;
+    int32 count;
+    b2PolygonShape Shape;
+
     b2FixtureDef fixtureDef;
 };
 
-void DrawBox(b2Body* &body){
-    const SDL_FRect R={ 
-        camera.x+body->GetPosition().x*camera.zoom,
-        camera.y+body->GetPosition().y*camera.zoom,
-        1.0f,1.0f
+void LTE_DrawShapes(Object* object){
+    b2Vec2 Pos = object->body->GetPosition();
+    float Angle = object->body->GetAngle();
+    
+    for(int i=0;i<object->count;i++){
+        float x1 = camera.x+(Pos.x+object->vertices[i].x)*camera.zoom; 
+        float y1 = camera.y+(Pos.y+object->vertices[i].y)*camera.zoom; 
+        float x2 = camera.x+(Pos.x+object->vertices[(i+1)%object->count].x)*camera.zoom; 
+        float y2 = camera.y+(Pos.y+object->vertices[(i+1)%object->count].y)*camera.zoom;
+        SDL_RenderLine(renderer, x1, y1, x2, y2);
+    }
+    // To be continued
+}
+/*
+void DrawBox(Object* box){
+
+    b2Vec2 pos = box->body->GetPosition();
+    float angle = box->body->GetAngle();
+
+    SDL_FRect Rect =  {
+        camera.x+camera.w/2+pos.x*camera.zoom,
+        camera.h-(camera.y+camera.h/2+pos.y*camera.zoom),
+        box->w*camera.zoom,
+        box->h*camera.zoom
     };
     
-}
+    SDL_RenderRect(renderer, &Rect);
+}*/
 int Box2d(){
     b2BodyDef groundBodyDef;
     groundBodyDef.position.Set(0.0f, -10.0f);
@@ -76,6 +99,43 @@ int Box2d(){
 
     groundBody->CreateFixture(&groundBox, 0.0f);
 
+    return 0;
+}
+
+int Init();
+void Destroy();
+
+int main(int argc, char* argv[]) {
+    if(Init() != 0){
+        return -1;
+    }
+
+    Box2d();
+    //Object box1(&world,0.0f,4.0f,1.0f,1.0f,10.0f,0.3f);
+    //Object box2(&world,1.0f,4.0f,1.0f,1.0f,10.0f,0.3f);
+
+    while(keep){
+        SDL_PollEvent(&event);
+        if(event.type == SDL_EVENT_QUIT){
+            keep = false;
+            break;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0,0,0,255);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+
+        world.Step(timeStep, velocityIterations, positionIterations);
+        
+        //DrawBox(&box1); 
+        //DrawBox(&box2);
+
+        SDL_RenderPresent(renderer);
+        SDL_Log("Event: %d", event.type);
+        SDL_Delay(50);
+    }
+
+    Destroy();
     return 0;
 }
 
@@ -114,25 +174,4 @@ void Destroy(){
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
     SDL_Quit();
-}
-
-int main(int argc, char* argv[]) {
-    if(Init() != 0){
-        return -1;
-    }
-
-    while(keep){
-        SDL_PollEvent(&event);
-        if(event.type == SDL_EVENT_QUIT){
-            keep = false;
-            break;
-        }
-        
-        SDL_RenderPresent(renderer);
-        SDL_Log("Event: %d", event.type);
-        SDL_Delay(50);
-    }
-
-    Destroy();
-    return 0;
 }
